1d033f46b8a8842bac99f1ebb23fb303a6a8f434
parip <fateme.it89@gmail.com>
2021-05-18 12:14:00 +0430

    implement cerror for manage errors in project test the cerrors function

---
+++ pkg/cerror/cerror.go
@@ -0,0 +1,66 @@
+package cerror
+
+import (
+	"errors"
+	"net/http"
+)
+
+type (
+	kind uint
+
+	serverError struct {
+		kind    kind
+		message string
+	}
+)
+
+const (
+	_ kind = iota
+	KindInvalid
+	KindNotFound
+	KindUnauthorized
+	KindUnexpected
+	KindNotAllowed
+)
+
+var (
+	httpErrors = map[kind]int{
+		KindInvalid:      http.StatusBadRequest,
+		KindNotFound:     http.StatusNotFound,
+		KindUnauthorized: http.StatusUnauthorized,
+		KindUnexpected:   http.StatusInternalServerError,
+		KindNotAllowed:   http.StatusMethodNotAllowed,
+	}
+)
+
+//New is constructor of the errors package
+func New(kind kind, msg string) error {
+	return serverError{
+		kind:    kind,
+		message: msg,
+	}
+}
+
+//Error return message of error
+func (e serverError) Error() string {
+	return e.message
+}
+
+/*HttpError convert kind of error to Http status error
+if error type is not serverError return 400 status code */
+func HttpError(err error) (string, int) {
+	var serverErr serverError
+
+	ok := errors.As(err, &serverErr)
+	if !ok {
+		return err.Error(), http.StatusBadRequest
+	}
+
+	code, ok := httpErrors[serverErr.kind]
+	if !ok {
+		return serverErr.message, http.StatusBadRequest
+	}
+
+	return serverErr.message, code
+
+}
---
+++ pkg/cerror/cerror_test.go
@@ -0,0 +1,55 @@
+package cerror
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHttpError(t *testing.T) {
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 int
+	}{
+		// TODO: Add test cases.
+		{
+			name:  "server error",
+			args:  args{err: New(KindNotFound, "this page note found")},
+			want:  "this page note found",
+			want1: http.StatusNotFound,
+		},
+		{
+			name: "server error",
+			args: args{
+				err: fmt.Errorf("something: %w", New(KindNotFound, "page not found")),
+			},
+			want:  "page not found",
+			want1: http.StatusNotFound,
+		},
+		{
+			name: "other errors",
+			args: args{
+				err: errors.New("something"),
+			},
+			want:  "something",
+			want1: http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := HttpError(tt.args.err)
+			if got != tt.want {
+				t.Errorf("HttpError() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("HttpError() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
