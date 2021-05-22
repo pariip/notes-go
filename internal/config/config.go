7c49cee53c60ceca9f0e6640a8c33eab384d588e
parip <fateme.it89@gmail.com>
2021-05-22 15:21:28 +0430

    add config for logger in build/config and internal/config/config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ type (
 	Config struct {
 		Database Database `yaml:"database"`
 		I18n     I18n     `yaml:"i18n"`
+		Logger   Logger   `yaml:"logger"`
 	}
 	Database struct {
 		Postgres Postgres `yaml:"postgres"`
@@ -22,5 +23,13 @@ type (
 	}
 	I18n struct {
 		BundlePath string `yaml:"bundle_path"`
+	}
+	Logger struct {
+		MaxAge          string `yaml:"max_age"`
+		MaxSize         string `yaml:"max_size"`
+		FilenamePattern string `yaml:"filename_pattern"`
+		RotationTime    string `yaml:"rotation_time"`
+		InternalPath    string `yaml:"internal_path"`
+		RequestPath     string `yaml:"request_path"`
 	}
 )
