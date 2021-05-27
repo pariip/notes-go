57772f96e9af4c48b93d3b851660d3184a690e20
parip <fateme.it89@gmail.com>
2021-05-27 15:59:37 +0430

    implement curd admin user role and routers

---
+++ internal/http/server/routes.go
@@ -0,0 +1,9 @@
+package server
+
+func (s *httpServer) setRoutes() {
+	s.admin.POST("/user", s.handler.adminCreateUser)
+	s.admin.GET("/user/:id", s.handler.adminGetUser)
+	s.admin.PATCH("/user", s.handler.adminUpdateUser)
+	s.admin.DELETE("/user/:id", s.handler.adminDeleteUser)
+
+}
