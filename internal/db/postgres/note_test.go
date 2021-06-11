b0a663b02effbeaf88ca2acd7ba7dec51df1b067
parip <fateme.it89@gmail.com>
2021-06-11 10:28:52 +0430

    add repository layer for GetAllMyNotes

--- internal/db/postgres/note_test.go
+++ internal/db/postgres/note_test.go
@@ -44,7 +44,87 @@ func TestCreateNote(t *testing.T) {
 	}
 }
 
-func TestRepository_GetNoteByID(t *testing.T) {
+func TestGetAllNotes(t *testing.T) {
+	setupTest(t)
+	defer teardownTest()
+
+	user := newUserTest()
+	var err error
+	t.Run("create new user", func(t *testing.T) {
+		if user, err = repositoryTest.CreateUser(user); err != nil {
+			t.Fail()
+		}
+	})
+	note := newNoteTest(user)
+	t.Run("create new note", func(t *testing.T) {
+		if note, err = repositoryTest.CreateNote(note); err != nil {
+			t.Fail()
+		}
+	})
+	tests := []struct {
+		name   string
+		userID uint
+		want   error
+	}{
+		{
+			name:   "get all note for userID",
+			userID: note.UserID,
+			want:   nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err = repositoryTest.GetAllNotes(tt.userID)
+			if !errors.Is(err, tt.want) {
+				t.Error()
+			}
+		})
+	}
+
+}
+
+func TestGetAllMyNotes(t *testing.T) {
+	setupTest(t)
+	defer teardownTest()
+
+	user := newUserTest()
+	var err error
+	t.Run("create new user", func(t *testing.T) {
+		if user, err = repositoryTest.CreateUser(user); err != nil {
+			t.Fail()
+		}
+	})
+	note := newNoteTest(user)
+	t.Run("create new note", func(t *testing.T) {
+		if note, err = repositoryTest.CreateNote(note); err != nil {
+			t.Fail()
+		}
+	})
+	tests := []struct {
+		name   string
+		userID uint
+		want   error
+	}{
+		{
+			name:   "get all note for userID",
+			userID: note.UserID,
+			want:   nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err = repositoryTest.GetAllMyNotes(tt.userID)
+			if !errors.Is(err, tt.want) {
+				t.Error()
+			}
+		})
+	}
+
+}
+
+func TestGetNoteByID(t *testing.T) {
 	setupTest(t)
 	defer teardownTest()
 	user := newUserTest()
@@ -89,47 +169,7 @@ func TestRepository_GetNoteByID(t *testing.T) {
 
 }
 
-func TestRepository_GetAllNotes(t *testing.T) {
-	setupTest(t)
-	defer teardownTest()
-
-	user := newUserTest()
-	var err error
-	t.Run("create new user", func(t *testing.T) {
-		if user, err = repositoryTest.CreateUser(user); err != nil {
-			t.Fail()
-		}
-	})
-	note := newNoteTest(user)
-	t.Run("create new note", func(t *testing.T) {
-		if note, err = repositoryTest.CreateNote(note); err != nil {
-			t.Fail()
-		}
-	})
-	tests := []struct {
-		name   string
-		userID uint
-		want   error
-	}{
-		{
-			name:   "get all note for userID",
-			userID: note.UserID,
-			want:   nil,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err = repositoryTest.GetAllNotes(tt.userID)
-			if !errors.Is(err, tt.want) {
-				t.Error()
-			}
-		})
-	}
-
-}
-
-func TestRepository_UpdateNote(t *testing.T) {
+func TestUpdateNote(t *testing.T) {
 	setupTest(t)
 	defer teardownTest()
 
@@ -179,7 +219,7 @@ func TestRepository_UpdateNote(t *testing.T) {
 	}
 }
 
-func TestRepository_DeleteNote(t *testing.T) {
+func TestDeleteNote(t *testing.T) {
 	setupTest(t)
 	defer teardownTest()
 
